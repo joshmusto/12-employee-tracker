c6b6da7092f1c73a2acd1a0bbe88e4a60bd3d4d8
Josh Musto <musto.josh@gmail.com>
2023-02-06 13:22:46 -0500

    pass03

--- server.js
+++ server.js
@@ -27,44 +27,65 @@ const connection = mysql.createConnection(
     console.log('Connected to staff_db')
 );
 
-
 // print database
 // Query database
-connection.query('SELECT * FROM department', function (err, results) {
-    //department
+/* connection.query('SELECT * FROM department', function (err, results) {
     console.log(results);
-  });
+  }); */
 
-//role
+//What would you like to do?
+    //view all employees
+    //add employee
+    //update employee role
+    //view all roles
+    //add role
+    //view all departments
+    //add department
 
-//employee
-
-//show options
-//view all departments
-//view all roles
-//view all employees
-//add department
-//add role
-//add employee
-//update employee role
-
-//view all departments
-//formatted table with names and department id's
-
-//view all roles
-//show job title, id, department, salary
+//customizable query function
+const customQuery = (dataTarget, tableTarget, idTarget) => {
+    connection.query(`SELECT ${dataTarget} FROM ${tableTarget} WHERE id = ${idTarget}`, function (err, results) {
+        return results;
+    })
+}
+console.log(customQuery("*", "role", "1"));
 
 //view all employees
 //formatted table with employee id's, first name, last name, job titles, departments, salaries, respective managers
-
-//add department
-//enter name, add to database
-
-//add role
-//enter name, salary, department, add to database
+const tableEmployees = () => {
+    connection.query('SELECT * FROM employee', function (err, results) {
+        console.table([
+            {
+                id: results[0].id,
+                first_name: results[0].first_name,
+                last_name: results[0].last_name,
+                title: customQuery("title", "role", results[0].role_id),
+                manager_id: results[0].manager_id,
+            }
+        ])
+    })
+}
+tableEmployees();
 
 //add employee
 //first name, last name, role, manager
 
+
 //update employee role
 //select employee, change role
+
+
+//view all roles
+//show job title, id, department, salary
+
+
+//add role
+//enter name, salary, department, add to database
+
+
+//view all departments
+//formatted table with names and department id's
+
+
+//add department
+//enter name, add to database
